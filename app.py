d9c64dee7da114b0775b012016e3743274ffae34
HanielFreitas <hanielfreitas3@gmail.com>
2025-06-02 13:43:34 -0400

    Parte 01

---
+++ app.py
@@ -0,0 +1,10 @@
+import requests
+
+def fetch_page(url):
+    response = requests.get(url)
+    return response.text
+
+if __name__ == "__main__":
+    url = "https://shopee.com.br/Rel%C3%B3gio-Com-Cord%C3%A3o-De-Folhas-Rel%C3%B3gios-Femininos-Pulseiras-Com-Diamantes-i.1025667949.23892777482"
+    page_content = fetch_page(url)
+    print(page_content)
